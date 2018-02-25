65a9b0a2ef0fb2948a77fd5157ee054e2a6088c5
aananth <akhilasananth@gmail.com>
2018-02-25 07:43:04 -0500

    seperated fetch location button from App.js

--- App/App.js
+++ App/App.js
@@ -1,58 +1,56 @@
+'use strict';
 import React , { Component } from 'react';
-import {AppRegistry,Image,StyleSheet, Text, View, Button,Alert} from 'react-native';
-import {StackNavigator} from 'react-navigation';
+import {Image,
+        StyleSheet, 
+        Text, 
+        View, 
+        Button,
+        Alert,
+        NavigatorIOS} from 'react-native';
+import FetchLocation from './Components/fetchLocation';
 
 // import Main from 'main';
 
 export default class App extends Component {
-  _handlePress() {
+  getUserLocationHandler = () => {
     Alert.alert('Thank you for your contribution!','Every little contribution helps charities better help people who are homeless')
   }
 
   render() {
-    const resizeMode = 'center';
-    const text = 'I am some centered text';
 
     return (
-      <View
-        style={{flex:1,flexDirection: 'row'}}
-      >
+      <View style={{flex:1,flexDirection: 'row'}}>
         <Image
-          style={{
-            resizeMode: 'cover',
-            position:'absolute',left:0,right:0,top:0,bottom:0,
-            width: null,
-            height: null,
-            zIndex: 100
-          }}
+          style={styles.backgroundImage}
           source={require('./images/cBackground.png')}/>
 
-          <View style={{
-            position:'absolute',
-            left:0,
-            right:0,
-            top:0,bottom:0,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 200
-          }}>
-            <Button onPress={this._handlePress} title="Log!" color="#46A4FB" />
-          </View>
-
+          <View style={styles.centerButton}>
+            <FetchLocation onGetLocation = {this.getUserLocationHandler} />
+          </View> 
      </View>
     );
   }
 }
 
-AppRegistry.registerComponent('BackgroundImage', () => BackgroundImage);
-
 const styles = StyleSheet.create({
-  fullContainer: {
-    flex: 1,
-    backgroundColor: '#eee',
-    justifyContent: 'center'
-  },
-});
 
+  centerButton: {
+    position:'absolute',
+    left:0,
+    right:0,
+    top:0,bottom:0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 200
+  },
+
+  backgroundImage:{
+    resizeMode: 'cover',
+    position:'absolute',left:0,right:0,top:0,bottom:0,
+    width: null,
+    height: null,
+    zIndex: 100
+  }
+});
 
