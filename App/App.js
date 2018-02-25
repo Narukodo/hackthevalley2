e07d9ccff1de4f64a6db2f5d9d93d1517d24c8d9
aananth <akhilasananth@gmail.com>
2018-02-24 22:18:06 -0500

    got a basic button on the image

--- App/App.js
+++ App/App.js
@@ -1,9 +1,13 @@
 import React , { Component } from 'react';
-import {AppRegistry,Image,StyleSheet, Text, View, Dimensions } from 'react-native';
+import {AppRegistry,Image,StyleSheet, Text, View, Button} from 'react-native';
 
 // import Main from 'main';
 
 export default class App extends Component {
+  _handlePress() {
+    console.log('Pressed!');
+  }
+
   render() {
     const resizeMode = 'center';
     const text = 'I am some centered text';
@@ -12,15 +16,29 @@ export default class App extends Component {
       <View
         style={{flex:1,flexDirection: 'row'}}
       >
-        <Image 
+        <Image
           style={{
             resizeMode: 'cover',
             position:'absolute',left:0,right:0,top:0,bottom:0,
-            height: null,
             width: null,
+            height: null,
+            zIndex: 100
           }}
-          source={require('./images/cBackground.png')} />
-      </View>
+          source={require('./images/cBackground.png')}/>
+
+          <View style={{
+            position:'absolute',
+            left:0,
+            right:0,
+            top:0,bottom:0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 200
+          }}>
+            <Button onPress={this._onPress} title="Hello" color="#FFFFFF"/>
+          </View>
+     </View>
     );
   }
 }
@@ -32,6 +50,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#eee',
     justifyContent: 'center'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    zIndex: 200
+  },
+  buttonContainer: {
+    backgroundColor: '#2E9298',
+    borderRadius: 10,
+    padding: 10,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.25
   }
 });
 
